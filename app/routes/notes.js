659f3f1b40903539e0bc1a51adf4e9c029a9d4b4
Andre Ferreira <andrebmx789@gmail.com>
2021-06-24 05:18:40 -0300

    fix: update mongoose to use findById

--- app/routes/notes.js
+++ app/routes/notes.js
@@ -5,13 +5,13 @@ const withAuth = require('../middlewares/auth');
 
 router.post('/', withAuth, async (req, res) => {
     const { title, body } = req.body;
-    
+
     try {
-        let note = new Note({title: title, body: body, author: req.user._id});
+        let note = new Note({ title: title, body: body, author: req.user._id });
         await note.save();
         res.status(200).json(note);
     } catch (error) {
-        res.status(500).json({error: 'A problem ocurred by creating a new note!'});
+        res.status(500).json({ error: 'A problem ocurred by creating a new note!' });
         console.error(error);
     }
 });
@@ -20,8 +20,8 @@ router.get('/search', withAuth, async (req, res) => {
     const { query } = req.query;
     try {
         let notes = await Note
-        .find({ author: req.user._id })
-        .find({ $text: { $search: query } });
+            .find({ author: req.user._id })
+            .find({ $text: { $search: query } });
         res.json(notes);
     } catch (error) {
         res.status(500).json({ error: error });
@@ -32,21 +32,21 @@ router.get('/:id', withAuth, async (req, res) => {
     try {
         const { id } = req.params;
         let note = await Note.findById(id);
-        if(isOwner(req.user, note))
+        if (isOwner(req.user, note))
             res.json(note);
         else
-            res.status(403).json({error: 'Permission denied'});
+            res.status(403).json({ error: 'Permission denied' });
     } catch (error) {
-        res.status(500).json({error: 'A problem ocurred by finding a note!'});
+        res.status(500).json({ error: 'A problem ocurred by finding a note!' });
     }
 });
 
 router.get('/', withAuth, async (req, res) => {
     try {
-        let notes = await Note.find({author: req.user._id});
+        let notes = await Note.find({ author: req.user._id });
         res.json(notes);
     } catch (error) {
-        res.status(500).json({error: 'A problem ocurred by finding the notes!'});
+        res.status(500).json({ error: 'A problem ocurred by finding the notes!' });
     }
 });
 
@@ -56,17 +56,17 @@ router.put('/:id', withAuth, async (req, res) => {
 
     try {
         let note = await Note.findById(id);
-        if(isOwner(req.user, note)) {
-            let note = await Note.findOneAndUpdate(id, 
-                { $set: { title: title, body: body} },
-                { upsert: true, new: true }
+        if (isOwner(req.user, note)) {
+            let note = await Note.findByIdAndUpdate(id,
+                { $set: { title: title, body: body } },
+                { new: true }    
             );
             res.json(note);
         }
         else
-            res.status(403).json({error: 'Permission denied!'})
+            res.status(403).json({ error: 'Permission denied!' })
     } catch (error) {
-        
+        res.status(500).json({ error: 'A problem ocurred by updating a note!' });
     }
 });
 
@@ -74,19 +74,19 @@ router.delete('/:id', withAuth, async (req, res) => {
     const { id } = req.params;
     try {
         let note = await Note.findById(id);
-        if(isOwner(req.user, note)) {
+        if (isOwner(req.user, note)) {
             await note.delete();
-            res.json({menssage: 'OK'}).status(204);
+            res.status(204).send();
         } else
-        res.status(403).json({error: 'Permission denied!'});
+            res.status(403).json({ error: 'Permission denied!' });
 
     } catch (error) {
-        res.status(500).json({error: 'A problem ocurred by deleting the notes!'});
+        res.status(500).json({ error: 'A problem ocurred by deleting the notes!' });
     }
 });
 
 const isOwner = (user, note) => {
-    if(JSON.stringify(user._id) == JSON.stringify(note.author._id))
+    if (JSON.stringify(user._id) == JSON.stringify(note.author._id))
         return true;
     else
         return false;
--- config/database.js
+++ config/database.js
@@ -4,7 +4,8 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/MyNotes', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true
+    useCreateIndex: true,
+    useFindAndModify: false
 })
 .then(() => console.log('Connection successful!'))
 .catch((err) => console.log(err));
