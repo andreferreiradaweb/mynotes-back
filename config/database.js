fed262342512261d950c5ef2ebeaba760580dcef
Andre Ferreira <andrebmx789@gmail.com>
2021-09-26 18:49:18 -0300

    update database url

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/MyNotes', {
+mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
