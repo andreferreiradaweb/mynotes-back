a2c592e91b1ab86c791f9dfa6712c73b833a0708
Andre Ferreira <andrebmx789@gmail.com>
2021-09-26 22:47:29 -0300

    refactor: update cors

--- app.js
+++ app.js
@@ -10,13 +10,8 @@ var notesRouter = require('./app/routes/notes');
 
 var app = express();
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
+app.use(cors())
+app.options('*', cors())
 
 app.use(logger('dev'));
 app.use(express.json());
