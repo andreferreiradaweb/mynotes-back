96f1afe35f1f2cb0e81f7dcca878bbae58b48247
Andre Ferreira <andrebmx789@gmail.com>
2021-09-26 23:49:14 -0300

    cors vazio

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const usersRouter = require('./app/routes/users');
 const notesRouter = require('./app/routes/notes');
 
 const app = express();
-app.use(cors({origin: '*'}));
+app.use(cors());
 
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -19,7 +19,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 app.use('/users', usersRouter);
 app.use('/notes', notesRouter);
