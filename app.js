c58af4844ce7a2e273095fc3cde35a4b6a3128d7
Andre Ferreira <andrebmx789@gmail.com>
2021-09-26 23:58:32 -0300

    refactor: to start

--- app.js
+++ app.js
@@ -10,15 +10,11 @@ const notesRouter = require('./app/routes/notes');
 const app = express();
 app.use(cors());
 
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  next();
-});
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/users', usersRouter);
 app.use('/notes', notesRouter);
