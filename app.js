c8482a5d95c7ce65a7e1ebb9701bab826c2abc05
Andre Ferreira <andrebmx789@gmail.com>
2021-09-26 22:35:03 -0300

    remove bodyparser

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ var app = express();
 app.use(cors());
 
 app.use(logger('dev'));
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
